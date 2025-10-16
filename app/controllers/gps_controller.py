57d27d529309e6f33bd1b81eb8890187ed2d9c96
info010 <belencagri06@gmail.com>
2025-10-16 13:55:59 +0300

    gps service hazılrandı yeni route eklendi, kod düzenlendi ve yorum satırları eklendi. Summary ile okunabilirlik arttırıldı.

--- app/controllers/gps_controller.py
+++ app/controllers/gps_controller.py
@@ -1,24 +1,33 @@
 from ..models.gps_model import GPSUpdateRequest, GPSData
-from ..utils.database_async import fetch_one, fetch_all
+from ..services import gps_service as svc
 
 async def upsert_location(req: GPSUpdateRequest):
     """
     Create if driver not exists; otherwise update instantly.
     """
-    query = """
-        INSERT INTO gps_table (driver_id, latitude, longitude, updated_at)
-        VALUES ($1, $2, $3, NOW())
-        ON CONFLICT (driver_id)
-        DO UPDATE SET
-            latitude = EXCLUDED.latitude,
-            longitude = EXCLUDED.longitude,
-            updated_at = NOW()
-        RETURNING driver_id, latitude, longitude, updated_at;
-    """
-    row = await fetch_one(query, req.driver_id, req.latitude, req.longitude)
-    return {"success": True, "data": dict(row)}
+    result, err = await svc.upsert_location(req.driver_id, req.latitude, req.longitude)
+
+    if err:
+        return { "succes": False, "message": err, "data": {} }
+    return { "succes": True, "message": "Updated location", "data": result}
 
 async def get_all_latest():
-    query = "SELECT * FROM gps_table ORDER BY updated_at DESC;"
-    rows = await fetch_all(query)
-    return {"success": True, "data": [dict(r) for r in rows]}
+    """
+    Get all drivers latest locations.
+    """
+    result, err = await svc.get_all_latest()
+
+    if err:
+        return { "succes": False, "message": err, "data": {} }
+    return { "succes": True, "message": "Latest locations retrieved", "data": result}
+
+async def get_latest(driver_id: str):
+    """
+    Get a driver's latest location.
+    """
+    result, err = await svc.get_latest(driver_id)
+    
+    if err:
+        return { "succes": False, "message": err, "data": {} }
+    return { "succes": True, "message": "Latest location retrieved", "data": result}
+
